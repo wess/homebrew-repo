49647e7ba6e1b4b28d46cf38adc9c5a8e84e6dac
Wess Cope <wcope@me.com>
2016-11-16 17:45:46 -0500

    Completely updated forumla

--- overlook.rb
+++ overlook.rb
@@ -1,13 +1,16 @@
+# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
-class Overlook < Formula 
-  version   "0.0.1"
-  desc      ""
-  homepage  ""
-  url       "https://github.com/wess/overlook/archive/master.tar.gz"
-  sha256    "d4fbe3e667ddb0cc1f33b653c831b074e816b7cbf9886c1c61a7c965e1993566"
+class Overlook < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/wess/overlook/archive/0.0.1.tar.gz"
+  version "0.0.1"
+  sha256 "c752b089a5bbaf699ce622b6f702f73bc36eff9c8faf6be9c411f09e075eaf11"
 
-  def install 
-    system "/usr/bin/env swift --configuration release"
+  def install
+    system "swift build -c release"
     system "mv .build/release/overlook overlook"
 
     bin.install "overlook"
