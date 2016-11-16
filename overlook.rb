1abbd0d593b5b90f9717dfb3b79bf2f44edbc924
Wess Cope <wcope@me.com>
2016-11-16 17:53:05 -0500

    Creating forumlas blow

--- overlook.rb
+++ overlook.rb
@@ -3,14 +3,15 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 
 class Overlook < Formula
-  desc ""
-  homepage ""
-  url "https://github.com/wess/overlook/archive/0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "c752b089a5bbaf699ce622b6f702f73bc36eff9c8faf6be9c411f09e075eaf11"
+  desc      "File monitoring"
+  homepage  "http://github.com/wess/overlook"
+  url       "https://github.com/wess/overlook/archive/0.0.1.tar.gz"
+  version   "0.0.1"
+  sha256    "c752b089a5bbaf699ce622b6f702f73bc36eff9c8faf6be9c411f09e075eaf11"
+  head      "https://github.com/wess/overlook.git"
 
   def install
-    system "swift build -c release"
+    system "unset CC; swift build -c release"
     system "mv .build/release/overlook overlook"
 
     bin.install "overlook"
