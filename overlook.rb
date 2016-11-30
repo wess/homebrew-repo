8c79082b9ba8c823335522a69eb15b8acf14ac64
Wess Cope <wcope@me.com>
2016-11-30 15:47:39 -0500

    overlook - Version correction

--- overlook.rb
+++ overlook.rb
@@ -6,7 +6,7 @@ class Overlook < Formula
   desc      "File monitoring"
   homepage  "http://github.com/wess/overlook"
   url       "https://github.com/wess/overlook/archive/0.1.3.tar.gz"
-  version   "0.1.2"
+  version   "0.1.3"
   sha256    "7bcc7193296db26c4ea9153c3c73eafd1c750618a2b0470b6ffec660b80c1aa0"
   head      "https://github.com/wess/overlook.git"
 
